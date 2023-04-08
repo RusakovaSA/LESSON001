5753eda3b20480d863d7580cfb8543bc8e7dd217
rusakova slavyana <blondi_sl@mail.ru>
2023-04-08 21:25:54 +0300

    Самая-большая-гиря

---
+++ Example006/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача с гирями
+
+int a = 4;
+int b = 7;
+int c = 1;
+int d = 3;
+int e = 5;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("Самая большая гиря весит ");
+Console.WriteLine (max);
