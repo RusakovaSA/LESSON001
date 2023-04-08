c2fbd83128f9a75ae4e3bfb932f118da8ca5dc82
rusakova slavyana <blondi_sl@mail.ru>
2023-04-08 21:09:11 +0300

    If_Else

---
+++ Example005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выполнение условий ИСТИНА и ЛОЖЬ - if or else
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "славяна")
+{
+    Console.WriteLine("Ура, это же СЛАВЯНА!!!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
